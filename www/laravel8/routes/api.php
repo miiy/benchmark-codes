d99094a80a423f2f9ba245391e971948bd6a51dd
miiy <miiy@users.noreply.github.com>
2020-09-24 17:05:02 +0800

    Update

--- www/laravel8/routes/api.php
+++ www/laravel8/routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', [\App\Http\Controllers\BenchmarkController::class, 'index']);
+Route::get('/redis', [\App\Http\Controllers\BenchmarkController::class, 'redis']);
